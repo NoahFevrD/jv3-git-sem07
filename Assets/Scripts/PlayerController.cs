1fe5e44b94bfa2a05e460022dd4b3c9613455c38
NoahFevrD <noahdeltor@gmail.com>
2023-10-18 16:00:08 -0400

    Ajout du Script Mouvement du Personnage

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    // ---------------------------------
+    // Variables
+    // ---------------------------------
+
+    [SerializeField] float speed;
+    Rigidbody playerRb;
+
+    // ---------------------------------
+    // Functions
+    // ---------------------------------
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        playerRb.AddForce(speed * transform.forward, ForceMode.Impulse);
+    }
+}
